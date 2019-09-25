2e13d38497592221cadd5ae5895ed4445e16d302
MG01972 <xinjunan@huijintech.com.cn>
2019-09-25 15:04:34 +0800

    新增方法-缓存

--- microservices/eureka_client/src/main/java/com/example/eureka_client/controller/CodeLibraryController.java
+++ microservices/eureka_client/src/main/java/com/example/eureka_client/controller/CodeLibraryController.java
@@ -2,8 +2,12 @@ package com.example.eureka_client.controller;
 
 import com.example.eureka_client.service.CodeLibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class CodeLibraryController {
     @Autowired
     private CodeLibraryService codeLibraryService;
@@ -12,10 +16,11 @@ public class CodeLibraryController {
      * 获取所有商品列表
      * @return
      */
-    @RequestMapping("getCodeNameByCodeNoAndItemNo")
+    @RequestMapping(value="getCodeNameByCodeNoAndItemNo", method = RequestMethod.GET)
+    @ResponseBody
     public Object list(){
         System.out.println("-----CodeLibraryController.getCodeNameByCodeNoAndItemNo---------------");
-        codeLibraryService.getCodeNameByCodeNoAndItemNo("","");
+        codeLibraryService.getCodeNameByCodeNoAndItemNo("ApplyChannel","00");
 
         return codeLibraryService.getCodeNameByCodeNoAndItemNo("ApplyChannel","00");
     }
