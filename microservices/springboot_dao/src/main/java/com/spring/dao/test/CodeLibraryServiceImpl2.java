ae366c6e7fe3f90404d973866a7e7436b03dabc6
MG01972 <xinjunan@huijintech.com.cn>
2019-09-25 17:57:41 +0800

    tj

--- microservices/springboot_dao/src/main/java/com/spring/dao/test/CodeLibraryServiceImpl2.java
+++ microservices/springboot_dao/src/main/java/com/spring/dao/test/CodeLibraryServiceImpl2.java
@@ -3,6 +3,9 @@ package com.spring.dao.test;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.spring.dao.entity.CodeLibrary;
 import com.spring.dao.mapper.CodeLibraryMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,18 +14,19 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class CodeLibraryServiceImpl2 {
 //    public class CodeLibraryServiceImpl extends ServiceImpl<CodeLibraryMapper, CodeLibrary>  implements CodeLibraryService {
-
+    Logger logger = LoggerFactory.getLogger(CodeLibraryServiceImpl2.class);
     @Autowired
     CodeLibraryMapper codeLibraryMapper;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
     public void getCodeNameByCodeNoAndItemNo(String codeNo, String itemNo) {
-        System.out.println("---------------获取 itemName 1-----------");
+        logger.info("---------------获取 itemName 1-----------");
         String name =  codeLibraryMapper.getCodeNameByCodeNoAndItemNo(codeNo,itemNo);
-        System.out.println("-------itemName1 = "+name);
+        logger.info("-------itemName1 = "+name);
 
 //        List<CodeLibrary> list = this.codeLibraryMapper.selectList(new QueryWrapper<>(CodeLibrary.builder()
 //                .codeno(codeNo)
