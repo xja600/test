d8b04fb6b5ea2072d4ea890b77739cfc92aed944
MG01972 <xinjunan@huijintech.com.cn>
2019-09-25 15:10:51 +0800

    tj

---
+++ microservices/springboot_dao/src/main/java/com/spring/dao/config/MybatisConfig.java
@@ -0,0 +1,24 @@
+package com.spring.dao.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@MapperScan(basePackages = {"com.spring.dao.mapper"})
+public class MybatisConfig {
+
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+}
